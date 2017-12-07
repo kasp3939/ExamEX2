0854cbd4033353200372e654bf9f0c0a8ccb536d
Kasper Duch Jensen <kasp3939@edu.eal.dk>
2017-12-07 10:29:30 +0100

    Add project files.

---
+++ ExamEX2/AuctionItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace ExamEX2
+{
+    [DataContract]
+    public class AuctionItem
+    {
+        [DataMember]
+        public int ItemNumber { get; set; }
+        [DataMember]
+        public string ItemDescription { get; set; }
+        [DataMember]
+        public int RatingPrice { get; set; }
+        [DataMember]
+        public decimal BidPrice { get; set; }
+        [DataMember]
+        public string BidCustomName { get; set; }
+        [DataMember]
+        public string BidCustomPhone { get; set; }
+        [DataMember]
+        public DateTime BidTimestamp { get; set; }
+    }
+}
---
+++ ExamEX2/Bid.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamEX2
+{
+    public class Bid
+    {
+        public int ItemNumber { get; set; }
+        public string CustomName { get; set; }
+        public string Phone { get; set; }
+        public decimal Price { get; set; }
+    }
+}
